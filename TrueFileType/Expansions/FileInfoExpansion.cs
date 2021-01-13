7bc91e2769d45fd1ca952a227264927b0518532c
justin <justin.chang.tw@outlook.com>
2021-01-13 13:40:33 +0800

    加入專案檔案。

---
+++ TrueFileType/Expansions/FileInfoExpansion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TrueFileType.Expansions
+{
+    public static class FileInfoExpansion
+    {
+        public static string GetFileType(this FileInfo fileInfo)
+        {
+            List<FileTypeCode> ImageTypes = new List<FileTypeCode>() {
+                new FileTypeCode{  Hex="FFD8", Type=".jpg" },
+                new FileTypeCode{  Hex="424D", Type=".bmp" },
+                new FileTypeCode{  Hex="474946", Type=".gif" },
+                new FileTypeCode{  Hex="89504E470D0A1A0A", Type=".png" },
+                new FileTypeCode{  Hex="68656963", Type=".heic" },
+
+            };
+            string hex = string.Empty;
+            using (Stream S = File.OpenRead(fileInfo.FullName))
+            {
+                for (int i = 0; i < 40; i++)
+                {
+                    hex += S.ReadByte().ToString("X2");
+                    var type = ImageTypes.FirstOrDefault(x => hex.EndsWith(x.Hex))?.Type;
+
+                    if (!String.IsNullOrEmpty(type))
+                    {
+                        return type;
+                    }
+                }
+            }
+            return "";
+        }
+    }
+
+    class FileTypeCode
+    {
+        public string Hex { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ TrueFileType/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TrueFileType.Expansions;
+
+namespace TrueFileType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string file = "";
+            if (args.ToString()!="")
+            {
+                file = @".\image\01.heic";
+            }
+
+            var fileInfo = new FileInfo(file);
+
+            var type = fileInfo.GetFileType();
+
+            Console.WriteLine(type);
+        }
+
+    }
+
+
+}
